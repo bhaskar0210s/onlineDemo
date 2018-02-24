47f80c32b9855da4e6791f7b4c6a98ff6039dcd1
vishnu <vishnu@shoptreeapp.com>
2018-02-24 11:35:50 +0530

    Resolve issues

--- src/main.js
+++ src/main.js
@@ -3,12 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-<<<<<<< HEAD
-=======
 import Vuefire from 'vuefire'
 import firebase from './services/firebase'
 import VueResource from 'vue-resource'
->>>>>>> Initial Commit
 import 'bootstrap/dist/css/bootstrap.css'
 import jQuery from 'jquery'
 global.jQuery = jQuery
@@ -16,22 +13,16 @@ require('bootstrap')
 var VueScrollTo = require('vue-scrollto')
 
 Vue.use(VueScrollTo)
-<<<<<<< HEAD
-=======
 Vue.use(VueResource)
 Vue.use(Vuefire)
->>>>>>> Initial Commit
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-<<<<<<< HEAD
-=======
   firebase: {
     cat: firebase.database.ref('cat').orderByChild('created_at')
   },
->>>>>>> Initial Commit
   router,
   template: '<App/>',
   components: { App }
